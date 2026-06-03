6be43ec861a75555c52a96259d2471b46e1be56f
COCCO ALESSANDRO s353655 <s353655@studenti.polito.it>
2026-06-03 21:35:07 +0200

    Implemented database, index.js, dao.js

---
+++ server/dao.js
@@ -0,0 +1,122 @@
+import sqlite from 'sqlite3'
+import crypto from 'crypto'
+
+const db = new sqlite.Database("database.sqlite", (err) => {
+    if (err) throw err;
+    console.log("Connected to the SQLite Database.");
+})
+
+// USER
+
+export const getUser = (username, password) => {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM users WHERE username = ?";
+        db.get(sql, [username], (err, row) => {
+            if (err) reject(err);
+            else if (row === undefined) {
+                resolve(false); // NOT FOUND USER
+            } else {
+                const user = {id: row.id, username: row.username};
+
+                crypto.scrypt(password, row.salt, 16, (err, hashedPassword) => {
+                    if (err) reject(err);
+                    if (!crypto.timingSafeEqual(Buffer.from(row.password, "hex"), hashedPassword)) {
+                        resolve (password); // WRONG PASSWORD
+                    } else {
+                        resolve (user); // LOGIN OK
+                    }
+                })
+            }
+        })
+    })
+}
+
+export const getUserById = (id) => {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT id, username FROM users WHERE id = ?";
+        db.get(sql, [id], (err, row) => {
+            if (err) reject(err);
+            else if (row === undefined) {
+                resolve({error: "User not found"})
+            }
+            else resolve(row);
+        })
+    })
+}
+
+
+// NETWORK (stations, lines, segments)
+
+export const getStations = () => {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM stations";
+        db.all(sql, [], (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+        })
+    })
+}
+
+export const getLines = () => {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM lines";
+        db.all(sql, [], (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+        })
+    })
+}
+
+export const getSegments = () => {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT stationA, stationB, line_id as lineId FROM segments";
+        db.all(sql, [], (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+        })
+    })
+}
+
+
+// EVENTS
+
+export const getEvents = () => {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM events";
+        db.all(sql, [], (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+        }) 
+    })
+}
+
+
+// RANKING
+
+export const getRanking = () => {
+    return new Promise((resolve, reject) => {
+        const sql = `
+        SELECT users.username, games.score
+        FROM users, games
+        WHERE users.id = games.user_id
+        ORDER BY games.score DESC
+        `
+        db.all(sql, [], (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+        })
+    })
+}
+
+
+// SAVE THE GAME
+
+export const saveGame = (userId, score) => {
+    return new Promise((resolve, reject) => {
+        const sql = "INSERT INTO games (user_id, score) VALUES (?, ?)";
+        db.run(sql, [userId, score], function(err) {
+            if (err) reject(err);
+            else resolve(this.lastID);
+        })
+    })
+}
--- server/index.js
+++ server/index.js
@@ -1,9 +1,120 @@
 // imports
 import express from "express";
+import morgan from 'morgan';
+import cors from 'cors';
+import * as dao from './dao.js';
+
+import passport from "passport";
+import LocalStrategy from 'passport-local';
+import session from 'express-session';
 
 // init express
-const app = new express();
+const app = express();
 const port = 3001;
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessState: 200,
+  credentials: true
+}
+
+app.use(cors(corsOptions));
+
+// passport configuration
+passport.use(new LocalStrategy(
+  async function(username, password, cb) {
+    const user = await dao.getUser(username, password);
+
+    if (!user) {
+      return cb(null, false, "Incorrect username or password");
+    }
+    return cb(null, user);
+  }
+))
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (user, cb) {
+  return cb(null, user);
+});
+
+const isLoggedIn = (req, res, next) => {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  console.log(req.user)
+  return res.status(401).json({error: "Not authorized"});
+}
+
+app.use(session({
+  secret: "shhhhh... it's a secret!",
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.authenticate("session"));
+
+
+// AUTHENTICATION ROUTES
+
+// 1. POST /api/sessions - Login
+app.post("/api/sessions", passport.authenticate("local"), function(req, res) {
+  return res.status(201).json(req.user);
+});
+
+// 2. GET /api/sessions/current
+app.get("/api/sessions/current", (req, res) => {
+  if(req.isAuthenticated()) {
+    res.json(req.user);}
+  else
+    res.status(401).json({error: "Not authenticated"});
+});
+
+// 3. DELETE /api/sessions/current
+app.delete("/api/sessions/current", (req, res) => {
+  req.logout(() => {
+    res.end();
+  });
+});
+
+
+// GAME ROUTES
+
+// 1. GET /api/network
+app.get('/api/network', async (req, res) => {
+  try {
+    const [stations, lines, segments] = await Promise.all([
+      dao.getStations(),
+      dao.getLines(),
+      dao.getSegments()
+    ]);
+
+    res.json({ stations, lines, segments} );
+  } catch (err) {
+      console.error("Error in getting the network.", err);
+      res.status(500).end();
+  }
+})
+
+
+// 2. GET /api/ranking
+
+app.get("/api/ranking", async (req, res) => {
+  try {
+    const ranking = await dao.getRanking();
+    res.json(ranking);
+  } catch (err) {
+    console.error("Error in getting the ranking.", err);
+    res.status(500).end();
+  }
+})
+
 
 // activate the server
 app.listen(port, () => {
